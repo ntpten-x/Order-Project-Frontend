9741ff4df9d79c8e67c6f5375fde26977afa7ebd
ntpten-x <natthaphongcontact@gmail.com>
2026-02-06 01:05:16 +0700

    next

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "582dda222418eec6"; }
+/******/ 		__webpack_require__.h = function() { return "06da6b6d78bcd497"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
